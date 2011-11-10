a49cf52e95e64f47f27c91cd5a7f227d250d3fba
Andy Pike <andy@andypike.com>
2011-11-10 21:09:19 +0000

    Improved flower sample and adding a screenshot

--- flower.rb
+++ flower.rb
@@ -1,23 +1,42 @@
 require "kame"
 
-Kame.new(:paper => :pink) do 
-    forward 100
-    turn_right 90
-    forward 100
-    turn_left 90
-
-    colour :white 
-    pen_down
-  
-    def square
-      4.times do
-        forward 50 
-        turn_right 90
-      end
+Kame.new(:speed => 50) do 
+  def square
+    4.times do
+      forward 50 
+      turn_right 90
     end
+  end
+  
+  def stalk
+    colour :green
+    pen_down
+          
+    turn_right 90
+    forward 200
+    turn_left 90
+    forward 1
+    turn_left 90
+    forward 200
+  end
+  
+  def petals
+    colour :pink 
+    pen_down
     
     36.times do
       turn_right 10
       square
     end
+  end
+
+  # Move to the starting position
+  forward 320
+  turn_right 90
+  forward 100
+  turn_left 90
+  
+  # Draw the flower
+  stalk
+  petals
 end
